77415363e46d2a8c5fa1e9112254ae275dd1e83d
joma_ssi <john.akapo@samsung.com>
2016-10-13 06:02:12 -0700

    MEC-577 #comment Validate server certificate

--- src/main/java/cloud/artik/lwm2m/ArtikCloudClient.java
+++ src/main/java/cloud/artik/lwm2m/ArtikCloudClient.java
@@ -248,28 +248,8 @@ public class ArtikCloudClient {
 
         // Install the all-trusting trust manager
         try {
-            // Create a trust manager that does not validate certificate chains
-            TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        @Override
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return new X509Certificate[0];
-                        }
-
-                        @Override
-                        public void checkClientTrusted(
-                                X509Certificate[] certs, String authType) {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(
-                                X509Certificate[] certs, String authType) {
-                        }
-                    }
-            };
-
             sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            sslContext.init(null, null, null);
         } catch (Exception e) {
             //
         }
